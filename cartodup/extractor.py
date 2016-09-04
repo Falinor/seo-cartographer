52474680de3380b16a6e74a7ed28d1308dde1144
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2016-09-05 00:22:42 +0200

    feat(extractor): add 'connection lost' error handling

--- cartodup/extractor.py
+++ cartodup/extractor.py
@@ -1,14 +1,20 @@
 import requests
+import sys
 
 
 def extract(url: str) -> list:
-    r = requests.get(url)
-    if r.status_code == 200:
-        path = 'resources/' + url + '.html'
-        with open(path, 'wb') as f:
-            f.write(r.text.encode('UTF-8'))
-            # TODO: possibly detect write error
-        return path
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            path = 'resources/' + url + '.html'
+            with open(path, 'wb') as f:
+                f.write(r.text.encode('UTF-8'))
+                # TODO: possibly detect write error
+            return path
+    except requests.ConnectionError as ex:
+        msg = 'cartographer: {0} unreachable; this could be caused by a ' \
+              'loss of connection\n'.format(url)
+        print(msg, file=sys.stderr)
 
 
 if __name__ == '__main__':
