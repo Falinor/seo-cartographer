9b5e8ab8262188506fee2cf3913332b30dbc2791
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2016-09-04 21:41:45 +0200

    feat(extractor): add request to web page

--- cartodup/extractor.py
+++ cartodup/extractor.py
@@ -1,5 +1,12 @@
-def extract():
-    pass
+import requests
+
+
+def extract(url: str) -> list:
+    r = requests.get(url)
+    if r.status_code == 200:
+        with open(url + '.html', 'wb') as f:
+            f.write(r.text.encode('UTF-8'))
+
 
 if __name__ == '__main__':
-    extract()
+    pass
