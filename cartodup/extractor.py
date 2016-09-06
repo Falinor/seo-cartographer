71fdeb155f1a2ced397c8efff6f24dcc30ae1172
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2016-09-06 12:04:33 +0200

    fix(extractor): strip URL newline char

--- cartodup/extractor.py
+++ cartodup/extractor.py
@@ -1,18 +1,21 @@
+import re
 import requests
 import sys
 
 
-def extract(url: str) -> list:
+def extract(url: str) -> str:
     try:
+        url = url.strip('\n')
         r = requests.get(url)
         if r.status_code == 200:
+            url = re.sub(r'http(s)?://', '', url)
             path = 'resources/' + url + '.html'
             with open(path, 'wb') as f:
                 f.write(r.text.encode('UTF-8'))
                 # TODO: possibly detect write error
             return path
     except requests.ConnectionError as ex:
-        msg = 'cartographer: {0} unreachable; this could be caused by a ' \
+        msg = 'cartographer: {} unreachable; this could be caused by a ' \
               'loss of connection\n'.format(url)
         print(msg, file=sys.stderr)
 
