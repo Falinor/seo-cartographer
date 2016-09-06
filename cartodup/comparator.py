3a7a8e3dd938fe923cd4b1489857d21294a3b551
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2016-09-06 12:03:39 +0200

    feat(comparator): add class Comparator

--- cartodup/comparator.py
+++ cartodup/comparator.py
@@ -1,2 +1,27 @@
-def get_42():
-    return 42
+def compare(path: str):
+    """Compares given page content with other registered pages,
+    and populates a duplication matrix.
+
+    :param path: The path to a file containing what should be compared.
+    """
+    pass
+
+
+class Comparator:
+    def __init__(self):
+        self.matrix = {}
+
+    def add_result(self, url1: str, url2: str, result: float):
+        try:
+            self.matrix[url2][url1] = result
+        except KeyError:
+            if self.matrix.get(url1):
+                self.matrix[url1][url2] = result
+            else:
+                self.matrix[url1] = {url2: result}
+
+    def exists(self, url1: str, url2: str):
+        try:
+            return self.matrix[url1][url2] is not None
+        except KeyError:
+            return False
