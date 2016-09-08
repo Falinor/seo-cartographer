4072d684b92077b0db12b204f044b6a52b7acb5e
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2016-09-08 23:11:21 +0200

    test(extractor): replace tests

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,9 +1,31 @@
 import cartodup.extractor as extractor
 
-def test_extract():
-    url = 'https://google.fr'
-    path = extractor.extract(url)
-    assert path is not None
 
-    with open(path) as f:
-        assert f.readline() is not None
+HTTP_URL = 'http://www.speedtest.net/'
+HTTPS_URL = 'https://google.fr/'
+
+
+def test_strip_protocol():
+    url = extractor.URL(HTTP_URL)
+    expected = 'www.speedtest.net/'
+    actual = url.strip_protocol()
+
+    assert actual == expected
+
+
+def test_get_resource():
+    # HTTP URL
+    url = extractor.URL(HTTP_URL)
+    expected = 'resources/www-speedtest-net.html'
+    actual = url.get_resource()
+
+    assert actual is not None
+    assert actual == expected
+
+    # HTTPS URL
+    url = extractor.URL(HTTPS_URL)
+    expected = 'resources/google-fr.html'
+    actual = url.get_resource()
+
+    assert actual is not None
+    assert actual == expected
