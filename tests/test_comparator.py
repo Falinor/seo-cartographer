7ed2c9ab3be327d0ce7e98e9826e6fc7fb6552a9
Andrea Gueugnaut <andrea.gueugnaut@gmail.com>
2016-09-08 22:53:34 +0200

    test(comparator): add compare test

--- tests/test_comparator.py
+++ tests/test_comparator.py
@@ -1,5 +1,37 @@
-import cartographer.comparator as comparator
+import cartodup.comparator as comparator
+import cartodup.extractor as extractor
 
 
-def test_get():
-    assert comparator.get_42() == 42
+HTTP_URL = 'http://www.speedtest.net/'
+HTTPS_URL = 'https://google.fr/'
+
+
+def test_add():
+    url1 = extractor.extract(HTTP_URL)
+    comp = comparator.Comparator()
+    comp.add(url1)
+    assert len(comp.matrix) == 1
+    assert comp.matrix[url1.url] == {}
+
+    url2 = extractor.extract(HTTPS_URL)
+    comp.add(url2)
+    assert len(comp.matrix) == 2
+    assert comp.matrix[url2.url] == {}
+    assert comp.matrix[url1.url][url2.url] >= 0.0
+    assert comp.matrix[url1.url][url2.url] <= 1.0
+
+
+def test_matrix():
+    comp = comparator.Comparator()
+    assert comp.matrix is not None
+    assert comp.matrix == {}
+
+    url = comparator.URL(HTTP_URL)
+
+    comp.matrix[url.url] = 42
+    assert comp.matrix[url.url] == 42
+
+    url = comparator.URL(HTTPS_URL)
+
+    comp.matrix[url.url] = 21
+    assert comp.matrix[url.url] == 21
